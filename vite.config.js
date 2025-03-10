c20465cf8b8bfe2555e313740b85a0947799eba2
John Dave Rojo <rojo.dave2004@gmail.com>
2025-03-11 00:11:34 +0800

    change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/todo_app/",
   plugins: [react()],
-  base: "/todo_app",
+  build: {
+    outDir: "dist",
+  },
 });
